697390983de88ef2e71885ddeb9f884c2252cceb
Абдельнур Алекс Фатех <a.aabdelnur@mail.ru>
2023-07-15 00:49:42 +0300

    second commit

--- screens/AccountScreen.py
+++ screens/AccountScreen.py
@@ -4,9 +4,11 @@ from aengine_flask.screen import Screen
 from helpers import get_user
 
 
-class HomeScreen(Screen):
+class AccountScreen(Screen):
     def main(self):
         user = get_user(request)
         if user:
-            return render_template("index.html", data=New.query.all(), user=user)
+            query = New.query.all()
+            ids = [i.owner_id for i in query]
+            return render_template("account.html", data=query, user=user, ids=ids)
         return redirect("/login")
